b766f5bcd2f19cdcaae16e068b48e0d425f0b03a
Wonseok An <150128897+MintLocket@users.noreply.github.com>
2024-05-30 13:12:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response
 import os
 import csv
 
@@ -6,7 +6,6 @@ app = Flask(__name__)
 IMAGE_UPLOAD_FOLDER = 'static/pics'
 CSV_UPLOAD_FOLDER = 'static/data'
 
-# Ensure the upload folders exist
 os.makedirs(IMAGE_UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(CSV_UPLOAD_FOLDER, exist_ok=True)
 
@@ -41,22 +40,28 @@ def upload_file():
             return 'Unsupported file type'
 
 def merge_csv(new_file, existing_file_path):
-    new_data = list(csv.reader(new_file.stream.read().decode('utf-8').splitlines()))
+    new_data = csv.reader(new_file.stream.read().decode('utf-8').splitlines())
     existing_data = []
     
     with open(existing_file_path, 'r', newline='', encoding='utf-8') as existing_file:
         reader = csv.reader(existing_file)
         existing_data = list(reader)
-    
-    existing_data_set = set(map(tuple, existing_data))
-    new_data_set = set(map(tuple, new_data))
-    
-    merged_data_set = existing_data_set.union(new_data_set)
-    
-    with open(existing_file_path, 'w', newline='', encoding='utf-8') as existing_file:
+
+    with open(existing_file_path, 'a', newline='', encoding='utf-8') as existing_file:
         writer = csv.writer(existing_file)
-        for row in merged_data_set:
+        for row in new_data:
             writer.writerow(row)
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force the latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
