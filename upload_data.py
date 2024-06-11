71eaff6d767eb337c1ac6c619c3d755dcc274595
Wonseok An <150128897+MintLocket@users.noreply.github.com>
2024-06-11 14:26:32 +0900

    Update upload_data.py

--- upload_data.py
+++ upload_data.py
@@ -1,17 +1,22 @@
 import requests
 import os
+from datetime import datetime
+
+# 현재 날짜와 시간 가져오기
+now = datetime.now()
 
 #'서버 주소/upload 입력'
 url = '[서버 주소]/upload'
 
 # CSV 데이터 전송
-# '이미지'를 전송할 이미지 이름으로 변경
+# '이미지'를 전송할 이미지 이름으로 변경 (jpg or png)
 # 위반 시간과 csv 파일 이름은 다음 양식을 반드시 지켜야 함. ex) '2024-05-30 10:00:00', '2024-05-30.csv'
-csv_data = ['이미지.JPG', '년-월-일 시:분:초']
-csv_filename = '년-월-일.csv'
+current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+csv_data = ['이미지.jpg', current_time]
+csv_filename = now.strftime("%Y-%m-%d") + '.csv'
 
 # 이미지 파일 경로
-image_file_path = '이미지.JPG'
+image_file_path = '이미지.jpg'
 
 if os.path.isfile(image_file_path):
     with open(image_file_path, 'rb') as image_file:
